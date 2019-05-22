c23b8d310c9bfe819a6318fd5394c521c0dd84f8
L1x3r <yurij_k@inbox.lv>
2019-05-22 19:43:16 +0300

    Added possibility to change description Fixed show product

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -9,6 +9,16 @@ import java.util.Scanner;
 
 class ShoppingListApplication {
 
+    private static void printProduct(Product p) {
+        System.out.println(p.getId()+" | "
+                +p.getName()+" | "
+                +p.getPrice()+" | "
+                +p.getType().toString()+" | "
+                +p.getDiscount()+" | "
+                +p.getActualPrice()+" | "
+                +p.getDescription());
+    }
+
     public static void main(String[] args) {
         Map<Long, Product> productRepository = new HashMap<>();
         Long productIdSequence = 1L;
@@ -16,13 +26,17 @@ class ShoppingListApplication {
             Scanner scanner = new Scanner(System.in);
             try {
                 System.out.println("1. Create product");
-                System.out.println("2. Find product by id");
-                System.out.println("3. Exit");
+                System.out.println("2. Show information about product by id");
+                System.out.println("3. Change product description ");
+                System.out.println("4. Exit");
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 Boolean validation = null;
                 String name;
                 BigDecimal price;
                 BigDecimal discount;
+                String description;
+                Long currProductId;
+                Product currProduct;
                 Validator NewValidator = new Validator();
                 switch (userInput) {
                     case 1:
@@ -31,7 +45,7 @@ class ShoppingListApplication {
                             name = scanner.nextLine();
                             validation=NewValidator.ValidateName(name);
                             //System.out.println(validation);
-                        }while (validation==false);
+                        }while (!validation);
 
 
                             System.out.println("Enter product price: ");
@@ -41,7 +55,7 @@ class ShoppingListApplication {
                             System.out.println("Enter product discount: ");
                             discount=new BigDecimal(scanner.nextLine());
                             validation=NewValidator.ValidateDiscount(discount);
-                        }while (validation==false);
+                        }while (!validation);
 
                             System.out.println("Enter product category: ");
                         for (ProductTypes pt: ProductTypes.values()) {
@@ -50,6 +64,8 @@ class ShoppingListApplication {
                         userInput = Integer.valueOf(scanner.nextLine());
                         ProductTypes one = ProductTypes.getProductById(userInput);
 
+                        System.out.println("Enter product description (Optional): ");
+                        description=scanner.nextLine();
 
                         Product product = new Product();
                         product.setName(name);
@@ -57,17 +73,33 @@ class ShoppingListApplication {
                         product.setDiscount(discount);
                         product.setId(productIdSequence);
                         product.setType(one);
+                        product.setDescription(description);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
                         System.out.println("Result: " + product.getId());
                         break;
                     case 2:
                         System.out.println("Enter product id: ");
-                        long id = scanner.nextLong();
-                        Product findProductResult = productRepository.get(id);
-                        System.out.println(findProductResult);
+                        currProductId = Long.valueOf(scanner.nextLine());
+                        if (productRepository.containsKey(currProductId)) {
+                            currProduct = productRepository.get(currProductId);
+                            printProduct(currProduct);
+                        } else
+                            System.out.println("Coldn't find product with id = "+currProductId.toString());
                         break;
                     case 3:
+                        System.out.println("Enter product id: ");
+                        currProductId = Long.valueOf(scanner.nextLine());
+                        if (productRepository.containsKey(currProductId)) {
+                            currProduct = productRepository.get(currProductId);
+                            System.out.println("Current product description: ");
+                            System.out.println(currProduct.getDescription());
+                            System.out.println("Enter new product description: ");
+                            currProduct.setDescription(scanner.nextLine());
+                        } else
+                            System.out.println("Coldn't find product with id = "+currProductId.toString());
+                        break;
+                    case 4:
                         return;
                 }
             } catch (Exception e) {
