949de53b87e1bc66333a8e3738ca5401f30932ab
L1x3r <yurij_k@inbox.lv>
2019-05-22 18:19:58 +0300

    Added ProductTypes Added product fields

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -1,5 +1,6 @@
 package com.javaguru.shoppinglist;
 
+
 import java.math.BigDecimal;
 
 public class Product {
@@ -7,29 +8,39 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private String description;
+    private BigDecimal discount;
+    private BigDecimal actualPrice;
+    private ProductTypes type;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) { this.id = id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+    public BigDecimal getPrice() { return price; }
 
     public void setPrice(BigDecimal price) {
-        this.price = price;
+        this.price = price.setScale(2, BigDecimal.ROUND_HALF_UP);
     }
 
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+        this.actualPrice = price.subtract(price.multiply(discount.divide(BigDecimal.valueOf(100.00),2, BigDecimal.ROUND_HALF_UP))).setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public BigDecimal getDiscount() { return discount; }
+
+    public BigDecimal getActualPrice() { return actualPrice; }
+
+    public void setType(ProductTypes type) { this.type = type; }
+
+    public ProductTypes getType() { return type; }
 }
---
+++ src/com/javaguru/shoppinglist/ProductTypes.java
@@ -0,0 +1,25 @@
+package com.javaguru.shoppinglist;
+
+public enum ProductTypes {
+    FRUITS,
+    VEGETABLES,
+    MEAT,
+    MILK,
+    ALCOHOL,
+    OTHER;
+
+    public static ProductTypes getProductById(int id) {
+        for (ProductTypes pt: ProductTypes.values()) {
+            if (pt.ordinal() == id)
+                return pt;
+        }
+        return null;
+    }
+    public static int getProductTypesCount() {
+        int res=0;
+        for (ProductTypes pt: ProductTypes.values()) {
+            res++;
+        }
+        return res;
+    }
+}
